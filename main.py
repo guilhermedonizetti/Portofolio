25e6eb9f0ae8e8365093f86948b62b828b1823af
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-09-18 11:53:02 -0300

    Mudando para Flask.

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicial():
+	return render_template('index.html')
+
+@app.route('/templates/<string:pagina>')
+def mostrar_paginas(pagina):
+	extensao = pagina.split('.')
+	extensao = extensao[1]
+	if extensao=='html':
+		return render_template('{}'.format(pagina))
+	else:
+		return render_template('erro.html')
+
+if __name__ == '__main__':
+	app.run(Debug=True)
