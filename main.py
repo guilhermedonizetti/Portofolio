30a38843190524b1508b4865bae99015ebfd4a39
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-09-24 09:37:35 -0300

    Mostrando uma frase ao final da pagina.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from random import choice
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def erro(e):
 
 @app.route('/')
 def inicial():
-	return render_template('index.html')
+	frase = frases()
+	return render_template('index.html', frase=frase)
 
 @app.route('/templates/<string:pagina>')
 def mostrar_paginas(pagina):
@@ -18,6 +20,15 @@ def mostrar_paginas(pagina):
 		return render_template('{}'.format(pagina))
 	else:
 		return render_template('erro.html')
+
+def frases():
+	lista = [
+				['O insucesso é apenas uma oportunidade para recomeçar com mais inteligência.', 'Henry Ford'],
+				['Acredite que ainda não sorriu tudo e que seu coração ainda vai amar muito; acredite que o melhor da vida ainda vai chegar.', 'Desconhecido'],
+				['O problema não é o problema, o problema é a sua atitude sobre o problema.','Capitão Jack Sparrow']
+			]
+	frase = choice(lista)
+	return frase
 
 if __name__ == '__main__':
 	app.run(Debug=True)
