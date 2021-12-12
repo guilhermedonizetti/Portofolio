cda2e037fa2042196c2fa99110a00d2d9665bbbd
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-12-12 11:51:18 -0300

    Exibindo o tempo de experiencia dinamicamente.

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from flask import Flask, render_template
 from random import choice
+from datetime import datetime
+from pytz import timezone
+from math import floor
 
 app = Flask(__name__)
 
+#Retorna a pagina de erro
 @app.errorhandler(404)
 def erro(e):
 	return render_template('erro.html')
 
+#Retorna a pagina inicial
 @app.route('/')
 def inicial():
 	frase = frases()
 	return render_template('index.html', frase=frase)
 
+#Grencia as rotas das paginas
 @app.route('/templates/<string:pagina>')
 def mostrar_paginas(pagina):
 	extensao = pagina.split('.')
 	if extensao[1] == 'html':
 		if extensao[0] == 'pessoal':
 			foto = escolher_foto()
-			return render_template('{}'.format(pagina), foto=foto)
+			tempo = tempo_de_experiencia()
+			return render_template('{}'.format(pagina), foto=foto, tempo=tempo)
 		else:
 			return render_template('{}'.format(pagina))
 	else:
 		return render_template('erro.html')
 
+#Seleciona randomicamente uma frase para ser exibida na pagina inicial
 def frases():
 	lista = [
 				['O insucesso é apenas uma oportunidade para recomeçar com mais inteligência.', 'Henry Ford'],
@@ -34,11 +42,30 @@ def frases():
 	frase = choice(lista)
 	return frase
 
+#Seleciona rndomicamente uma foto para sre exibida na pagina de Sobre Mim
 def escolher_foto():
 	lista = ['eu_1', 'eu_1', 'eu_3']
 	foto = choice(lista)
 
 	return foto
+
+#Calcula a diferenca de datas para dizer automaticamente o tempo de experiencia
+def tempo_de_experiencia():
+	inicio_trabalho = datetime(2021, 12, 1)
+	fuso = timezone('America/Sao_Paulo')
+	data_atualmente = str(datetime.now().astimezone(fuso)).split(" ")
+	data_atualmente = data_atualmente[0].split("-")
+	data_atualmente =  datetime(int(data_atualmente[0]), int(data_atualmente[1]), int(data_atualmente[2]))
+	dias = data_atualmente - inicio_trabalho
+	dias = int(str(dias).split(" ")[0])
+
+	if dias >= 30:
+		mes = floor(dias / 30)
+		tempo = "{} meses".format(mes)
+	else:
+		tempo = "{} dias".format(dias)
+
+	return tempo
 
 if __name__ == '__main__':
 	app.run()
