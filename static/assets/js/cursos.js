a4a966b8893c399ee9eaf4a8090c8732584be26b
guilhermedonizetti <guilhermetecnologias@gmail.com>
2022-01-09 13:37:01 -0300

    Incluindo descricoes dos cursos realizados.

---
+++ static/assets/js/cursos.js
@@ -0,0 +1,43 @@
+//Curso 1
+
+$(document).ready(function() {
+	$('#curso1').click(function(){
+		$('#curso1_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso1_fechar').click(function(){
+		$('#curso1_desc').toggle(1000);
+	})
+});
+
+
+//Curso 2
+
+$(document).ready(function() {
+	$('#curso2').click(function(){
+		$('#curso2_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso2_fechar').click(function(){
+		$('#curso2_desc').toggle(1000);
+	})
+});
+
+
+//Curso 3
+
+$(document).ready(function() {
+	$('#curso3').click(function(){
+		$('#curso3_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso3_fechar').click(function(){
+		$('#curso3_desc').toggle(1000);
+	})
+});
