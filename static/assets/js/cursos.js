3f79d256ce4b3d9ef912eccc57705022c1c080de
guilhermedonizetti <guilhermetecnologias@gmail.com>
2022-01-15 09:42:33 -0300

    Alterado a forma como exibe a descricao dos cursos: oculta/exibe descricao.

--- static/assets/js/cursos.js
+++ static/assets/js/cursos.js
@@ -2,13 +2,15 @@
 
 $(document).ready(function() {
 	$('#curso1').click(function(){
-		$('#curso1_desc').show(1000);
+		$('#card_curso1').toggle(300);
+		$('#curso1_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso1_fechar').click(function(){
-		$('#curso1_desc').toggle(1000);
+		$('#curso1_desc').toggle(300);
+		$('#card_curso1').show(300);
 	})
 });
 
@@ -17,13 +19,15 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('#curso2').click(function(){
-		$('#curso2_desc').show(1000);
+		$('#card_curso2').toggle(300);
+		$('#curso2_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso2_fechar').click(function(){
-		$('#curso2_desc').toggle(1000);
+		$('#curso2_desc').toggle(300);
+		$('#card_curso2').show(300);
 	})
 });
 
@@ -32,13 +36,15 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('#curso3').click(function(){
-		$('#curso3_desc').show(1000);
+		$('#card_curso3').toggle(300);
+		$('#curso3_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso3_fechar').click(function(){
-		$('#curso3_desc').toggle(1000);
+		$('#curso3_desc').toggle(300);
+		$('#card_curso3').show(300);
 	})
 });
 
@@ -47,13 +53,15 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('#curso4').click(function(){
-		$('#curso4_desc').show(1000);
+		$('#card_curso4').toggle(300);
+		$('#curso4_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso4_fechar').click(function(){
-		$('#curso4_desc').toggle(1000);
+		$('#curso4_desc').toggle(300);
+		$('#card_curso4').show(300);
 	})
 });
 
@@ -61,13 +69,15 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('#curso5').click(function(){
-		$('#curso5_desc').show(1000);
+		$('#card_curso5').toggle(300);
+		$('#curso5_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso5_fechar').click(function(){
-		$('#curso5_desc').toggle(1000);
+		$('#curso5_desc').toggle(300);
+		$('#card_curso5').show(300);
 	})
 });
 
@@ -75,13 +85,15 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('#curso6').click(function(){
-		$('#curso6_desc').show(1000);
+		$('#card_curso6').toggle(300);
+		$('#curso6_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso6_fechar').click(function(){
-		$('#curso6_desc').toggle(1000);
+		$('#curso6_desc').toggle(300);
+		$('#card_curso6').show(300);
 	})
 });
 
@@ -89,12 +101,14 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('#curso7').click(function(){
-		$('#curso7_desc').show(1000);
+		$('#card_curso7').toggle(300);
+		$('#curso7_desc').show(300);
 	})
 });
 
 $(document).ready(function() {
 	$('#curso7_fechar').click(function(){
-		$('#curso7_desc').toggle(1000);
+		$('#curso7_desc').toggle(300);
+		$('#card_curso7').show(300);
 	})
 });
