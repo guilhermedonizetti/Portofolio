dd89c6bb63982dead55e33a98d3f92672ef1c86b
guilhermedonizetti <guilhermetecnologias@gmail.com>
2022-01-15 09:22:12 -0300

    Incluindo a descricao dos cursos e inserindo o curso de API.

--- static/assets/js/cursos.js
+++ static/assets/js/cursos.js
@@ -41,3 +41,60 @@ $(document).ready(function() {
 		$('#curso3_desc').toggle(1000);
 	})
 });
+
+
+//Curso 4
+
+$(document).ready(function() {
+	$('#curso4').click(function(){
+		$('#curso4_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso4_fechar').click(function(){
+		$('#curso4_desc').toggle(1000);
+	})
+});
+
+//Curso 5
+
+$(document).ready(function() {
+	$('#curso5').click(function(){
+		$('#curso5_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso5_fechar').click(function(){
+		$('#curso5_desc').toggle(1000);
+	})
+});
+
+//Curso 6
+
+$(document).ready(function() {
+	$('#curso6').click(function(){
+		$('#curso6_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso6_fechar').click(function(){
+		$('#curso6_desc').toggle(1000);
+	})
+});
+
+//Curso 7
+
+$(document).ready(function() {
+	$('#curso7').click(function(){
+		$('#curso7_desc').show(1000);
+	})
+});
+
+$(document).ready(function() {
+	$('#curso7_fechar').click(function(){
+		$('#curso7_desc').toggle(1000);
+	})
+});
